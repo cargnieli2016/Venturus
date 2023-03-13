2313a0487b07052904a8a9b68adaa63e5ce390eb
Cargnieli2016 <cargnieli2016@gmail.com>
2023-03-13 09:45:41 -0300

    Aula11Exemplo5

---
+++ JAVA/Aula11/Aula11Exemplo5.java
@@ -0,0 +1,12 @@
+public class Aula11Exemplo5 {
+    public static void main(String[] args) {
+        int M[][] = new int[2][2];
+        M[0][0] = 1;
+        M[0][1] = 4;
+        M[1][0] = 7;
+        M[1][1] = 9;
+
+        System.out.println(M[0][0] + "\t" + M[0][1]);
+        System.out.println(M[1][0] + "\t" + M[1][1]);
+    }
+}
